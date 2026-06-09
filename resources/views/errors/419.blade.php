9b4a54bfce3f723f813387776b634ba6dbde4192
Alejandro-SAM <alejandrotorresa01@gmail.com>
2026-06-09 16:55:10 -0600

    Merged QoL branch regarding error view exiting

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,5 @@
+@include('errors.layout', [
+    'code' => '419',
+    'title' => 'Session expired',
+    'message' => 'Your session has expired for security reasons. Please log in again to continue using the system.',
+])
