266685a194746046c3d9cc16a306f262276df9fc
Alejandro-SAM <alejandrotorresa01@gmail.com>
2026-06-22 09:48:59 -0600

    Revamped Profile data view

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -6,24 +6,21 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+
+            <!-- ONE SINGLE SECTION FOR PROFILE DETAILS -->
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
+
+                <!-- Account information -->
+                @include('profile.partials.update-profile-information-form')
+
+                <hr class="my-8 border-gray-200">
+
+                <!-- Password update -->
+                @include('profile.partials.update-password-form')
+
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div>
         </div>
     </div>
 </x-app-layout>
